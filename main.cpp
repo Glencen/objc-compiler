239d7679ee5a85c187993a5f743f6a841a01b941
Dmitry Silantiev <sylantiev21@gmail.com>
2025-11-21 02:14:54 +0300

    Created main function for parser

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include "objc_parser.hpp"
+
+namespace fs = std::filesystem;
+
+extern FILE *yyin;
+
+extern int yyparse();
+
+ProgramNode *root;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
+        return 1;
+    }
+
+    std::string inputFile = argv[1];
+
+    yyin = fopen(inputFile.c_str(), "r");
+    if (!yyin) {
+        cout << ("Could not open input file: '" + inputFile + "'");
+        return 1;
+    }
+
+    int parse_result = yyparse();
+
+    if (parse_result != 0) {
+        cout << ("Parsing failed with code: '" + std::to_string(parse_result) + "'");
+        return 1;
+    }
+
+    if (!root) {
+        cout << ("No parse tree generated");
+        return 1;
+    }
+
+    cout << root->toDot();
+
+    return 0;
+}
