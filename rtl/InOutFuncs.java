e79648fa1bf3fb0ba2ffd7e3798971ee92fd85a9
Zhovtobrukh Danil <dzhov123@mail.ru>
2026-01-24 19:45:49 +0300

    Rewrite .cpp to .java

---
+++ rtl/InOutFuncs.java
@@ -0,0 +1,88 @@
+package rtl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InOutFuncs {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    // ------------------- Print Methods -------------------
+
+    public static void printInt(int value) {
+        System.out.print(value);
+    }
+
+    public static void printFloat(float value) {
+        System.out.print(value);
+    }
+
+    public static void printChar(char value) {
+        System.out.print(value);
+    }
+
+    public static void printNSString(NSString str) {
+        System.out.print(str.toString());
+    }
+
+    public static void printNSNumber(NSNumber num) {
+        System.out.print(num.toString());
+    }
+
+    public static void printNSArray(NSArray arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.countDynamic(); i++) {
+            NSObject obj = arr.objectAtIndexDynamic(i);
+            if (obj != null) {
+                System.out.print(obj.toString());
+                if (i != arr.countDynamic() - 1) System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+
+    public static void printNSObject(NSObject obj) {
+        System.out.print(obj.toString());
+    }
+
+    // ------------------- Read Methods -------------------
+
+    public static int readInt() {
+        return scanner.nextInt();
+    }
+
+    public static float readFloat() {
+        return scanner.nextFloat();
+    }
+
+    public static char readChar() {
+        String s = scanner.next();
+        return s.isEmpty() ? '\0' : s.charAt(0);
+    }
+
+    public static NSString readNSString() {
+        String s = scanner.next();
+        return new NSString(s);
+    }
+
+    public static NSNumber readNSNumberInt() {
+        int value = scanner.nextInt();
+        return new NSNumber(value);
+    }
+
+    public static NSNumber readNSNumberFloat() {
+        float value = scanner.nextFloat();
+        return new NSNumber(value);
+    }
+
+    public static NSArray readNSArray() {
+        int n = scanner.nextInt();
+        List<NSObject> items = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String s = scanner.next();
+            items.add(new NSString(s));
+        }
+        return new NSArray(items);
+    }
+}
---
+++ rtl/NSNumber.java
@@ -0,0 +1,73 @@
+package rtl;
+
+// NSNumber.java
+public class NSNumber extends NSObject {
+
+    public enum NumberType { INT, FLOAT }
+
+    private NumberType type;
+    private int intValue;
+    private float floatValue;
+
+    // ------------------- Конструкторы -------------------
+    public NSNumber() {
+        super.initDynamic();
+        this.type = NumberType.INT;
+        this.intValue = 0;
+    }
+
+    public NSNumber(int value) {
+        super.initDynamic();
+        this.type = NumberType.INT;
+        this.intValue = value;
+    }
+
+    public NSNumber(float value) {
+        super.initDynamic();
+        this.type = NumberType.FLOAT;
+        this.floatValue = value;
+    }
+
+    // ------------------- Статические методы -------------------
+    public static NSNumber numberWithIntStatic(int value) {
+        return new NSNumber(value);
+    }
+
+    public static NSNumber numberWithFloatStatic(float value) {
+        return new NSNumber(value);
+    }
+
+    // ------------------- Динамические методы -------------------
+    public int intValueDynamic() {
+        if (type == NumberType.INT) return intValue;
+        return (int) floatValue;
+    }
+
+    public float floatValueDynamic() {
+        if (type == NumberType.FLOAT) return floatValue;
+        return (float) intValue;
+    }
+
+    @Override
+    public String descriptionDynamic() {
+        return type == NumberType.INT ? Integer.toString(intValue) : Float.toString(floatValue);
+    }
+
+    @Override
+    public NSNumber initDynamic() {
+        super.initDynamic();
+        return this;
+    }
+
+    // ------------------- Инициализация ClassInfo -------------------
+    public static void initClassInfo(ClassInfo info) {
+        NSNumber.s_classInfo = info;
+    }
+
+    // Статический ClassInfo для NSNumber
+    private static ClassInfo s_classInfo = null;
+
+    public static ClassInfo getClassInfoStatic() {
+        return s_classInfo;
+    }
+}
