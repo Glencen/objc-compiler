66f03d68181f25b2dd2c29de2a24d02c27836d6b
Dmitry Silantiev <sylantiev21@gmail.com>
2026-01-24 23:22:44 +0300

    Removed words from the list of reserved names that are not actually reserved words

--- context.cpp
+++ context.cpp
@@ -1284,22 +1284,15 @@ void SemanticContext::resolveInheritance() {
 
 void SemanticContext::initReservedNames() {
     reservedNames = {
-        "abstract", "assert", "boolean", "break", "byte",
-        "case", "catch", "char", "class", "const",
-        "continue", "default", "do", "double", "else",
-        "enum", "extends", "final", "finally", "float",
-        "for", "goto", "if", "implements", "import",
-        "instanceof", "int", "interface", "long", "native",
-        "new", "package", "private", "protected", "public",
-        "return", "short", "static", "strictfp", "super",
-        "switch", "synchronized", "this", "throw", "throws",
-        "transient", "try", "void", "volatile", "while"
+        "break", "case", "char", "const", "continue", "default", 
+        "do", "double", "else", "enum", "float", "for", "goto", 
+        "if", "int", "long", "return", "short", "switch", "void", 
+        "while"
     };
     
     reservedNames.insert({
         "id", "SEL", "IMP", "BOOL", "YES", "NO",
-        "nil", "Nil", "NULL", "self", "super",
-        "retain", "release", "autorelease", "copy"
+        "nil", "Nil", "NULL", "self", "super"
     });
 }
 
