9691f3649b41cd0d0f32824f6dba4b4f5afc0430
Zhovtobrukh Danil <dzhov123@mail.ru>
2026-01-26 00:33:27 +0300

    realize hex literals

---
+++ codex_tests/bytecode_hex_literals.m
@@ -0,0 +1,9 @@
+int main() {
+    int a = 0xFF;
+    int b = 0x1a3b;
+    [InOutFuncs printInt:a];
+    [InOutFuncs printChar:'\n'];
+    [InOutFuncs printInt:b];
+    [InOutFuncs printChar:'\n'];
+    return 0;
+}
